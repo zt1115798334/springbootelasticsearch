4c79e9e4a3220140ba40614139a75e4c647942e1
zhangtong <zhangtong9498@qq.com>
2017-09-11 14:23:31 +0800

    msg

--- src/main/java/com/zt/App.java
+++ src/main/java/com/zt/App.java
@@ -2,10 +2,14 @@ package com.zt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableSwagger2
+@ComponentScan(basePackages = {"com.zt"})
 public class App {
 
     public static void main(String[] args) {
